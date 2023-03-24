d9d2abfdb19f6c729d816da155e4e864df1a141b
Tatiana-pct <79706218+Tatiana-pct@users.noreply.github.com>
2023-03-24 15:16:56 +0100

    create PlayerWalk Anim

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,8 @@ public class Player : MonoBehaviour
     [SerializeField]private float _moveSpeed =10f;
     [SerializeField]private float _rotateSpeed =20f;
 
+    private bool _isWalking;
+
     private void Update()
     {
         Vector2 inputVector = new Vector2(0, 0);
@@ -35,7 +37,14 @@ public class Player : MonoBehaviour
         Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
         transform.position += moveDir * _moveSpeed * Time.deltaTime;
 
+        _isWalking = moveDir != Vector3.zero;
+
         transform.forward =  Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * _rotateSpeed);
 
+    }
+
+    public bool IsWalking()
+    {
+        return _isWalking;
     }
 }
