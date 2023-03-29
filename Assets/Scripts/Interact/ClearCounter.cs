ed9ab3d0a9bdd84b3acf71890273694e922b94ef
Tatiana-pct <79706218+Tatiana-pct@users.noreply.github.com>
2023-03-29 14:05:26 +0200

    create interact Function

---
+++ Assets/Scripts/Interact/ClearCounter.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearCounter : MonoBehaviour
+{
+
+    #region Intreact()
+    public void Intreact()
+    {
+        Debug.Log("Interact");
+    }
+
+    #endregion
+}
+
+#region
+#endregion
--- Assets/Scripts/PlayerScripts/Player.cs
+++ Assets/Scripts/PlayerScripts/Player.cs
@@ -6,12 +6,62 @@ public class Player : MonoBehaviour
     [SerializeField] private float _rotateSpeed = 20f;
     [SerializeField] private float _playerRadius = 0.7f;
     [SerializeField] private float _playerheight = 2f;
+    [SerializeField] private float _interactDistance = 2f;
     [SerializeField] private GameInput _gameInput;
+    [SerializeField] private LayerMask _CountersLayerMask;
 
 
     private bool _isWalking;
+    private Vector3 _lastInteractDir;
 
     private void Update()
+    {
+        HandleMouvement();
+        HandleInteractions();
+
+    }
+
+     #region IsWalking()
+    public bool IsWalking()
+    {
+        return _isWalking;
+    }
+    #endregion
+
+    #region HandleInteractions()
+    private void HandleInteractions()
+    {
+        Vector2 inputVector = _gameInput.GetMouvementVectorNormalized();
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
+
+        if(moveDir != Vector3.zero)
+        {
+            _lastInteractDir = moveDir;
+        }
+
+        RaycastHit hit;
+
+        if(Physics.Raycast(transform.position, moveDir,out hit, _interactDistance, _CountersLayerMask))
+        {
+            if(hit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                //Has ClearCounter
+                clearCounter.Intreact();
+            }
+
+           //ClearCounter clearCounter = hit.transform.GetComponent<ClearCounter>();
+           //if(clearCounter != null)
+           //{
+           //    // Has ClearCounter
+           //}
+        }
+       
+    }
+
+    #endregion
+
+    #region HandleMouvement()
+    private void HandleMouvement()
     {
 
         Vector2 inputVector = _gameInput.GetMouvementVectorNormalized();
@@ -21,7 +71,7 @@ public class Player : MonoBehaviour
 
         bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * _playerheight, _playerRadius, moveDir, _moveDistance);
 
-        if(!canMove)
+        if (!canMove)
         {
             //Cannot move toward moveDir
 
@@ -29,7 +79,7 @@ public class Player : MonoBehaviour
             Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
             canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * _playerheight, _playerRadius, moveDirX, _moveDistance);
 
-            if(canMove)
+            if (canMove)
             {
                 //Can only on the X
                 moveDir = moveDirX;
@@ -39,17 +89,17 @@ public class Player : MonoBehaviour
                 //Cannot move only the X
 
                 //Attemp only Z mvt
-                Vector3 moveDirZ = new Vector3(0,0,moveDir.z).normalized;
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
                 canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * _playerheight, _playerRadius, moveDirZ, _moveDistance);
-                
-                if(canMove)
-                    {
-                        moveDir = moveDirZ;
-                    }
-                    else
-                    {
-                        //Cannot move in any direction
-                    }
+
+                if (canMove)
+                {
+                    moveDir = moveDirZ;
+                }
+                else
+                {
+                    //Cannot move in any direction
+                }
 
             }
         }
@@ -64,11 +114,6 @@ public class Player : MonoBehaviour
         _isWalking = moveDir != Vector3.zero;
 
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * _rotateSpeed);
-
     }
-
-    public bool IsWalking()
-    {
-        return _isWalking;
-    }
+    #endregion
 }
